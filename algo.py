0f9f4d5314346111051b2a5ed2446061e8da6170
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2024-06-10 23:37:17 -0300

    Delete algo.py

--- algo.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("projeto-rpg.xlsx", "r") as arquivo:
-    arquiv_csv = csv.reader(arquivo, delimiter = ",")
-    for i, linha in enumerate(arquiv_csv):
-        if i == 0:
-            print("cabeçalho: " + str(linha))
-        else:
-            print("valor: " + str(linha))
