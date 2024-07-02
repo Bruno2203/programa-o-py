fb82ef31ab89a7aeb518506ea51e76e2ceae49fa
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2024-07-01 23:34:25 -0300

    Add files via upload

--- jogoo.py
+++ jogoo.py
@@ -1,15 +1,61 @@
+import csv
+
+# Inicializando a lista para armazenar as aventuras
 av = []
 
+# Lendo o conteúdo do arquivo 'objetivo.txt'
 with open('objetivo.txt', 'r') as arquivo:
     obj = arquivo.read()
 
+# Lendo o conteúdo do arquivo 'projeto.csv'
 with open('projeto.csv', 'r') as arquivo:
     aventura = csv.DictReader(arquivo)
     for i in aventura:
         av.append(i)
 
-def imprime_avntura(copia_v):
-    for h in copia_v:
-        print(f"av:{k[Aventuras:]}")
+# Função para imprimir todas as aventuras
+def imprime_aventura(copia_v):
+    for k in copia_v:
+        print(f"av: {k['Aventuras']}")
 
-def imprime_escolhida
+# Função para imprimir uma aventura específica
+def imprime_escolhida(copia_v, n):
+    for k in copia_v:
+        if k['Aventuras'] == n:
+            print(f"av: {k['Aventuras']}")
+
+# Função para excluir uma aventura específica
+def exclui_especifica(copia_v, n):
+    copia_v = [k for k in copia_v if k['Aventuras'] != n]
+    return copia_v
+
+def continua(palavra):
+    entrada = ""
+    while entrada != palavra:
+        entrada = input("Digite 's' para dar continuidade: ")
+        if entrada != palavra:
+            print("Palavra incorreta. Tente novamente.")
+    print("Palavra confirmada, autorização concedida")
+
+
+continua("s")
+print("O programa foi retomado.")
+print(obj)
+continua()
+imprime_aventura(av)
+av1 = input("escolha uma aventura! ")
+imprime_escolhida(av, av1)
+av = exclui_especifica(av, av1)
+print("FIM...")
+
+
+
+
+
+
+
+
+
+
+
+
