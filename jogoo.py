4cf1880de0a4a626316d28e21d3930e7bfe3d79d
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2024-07-23 22:43:47 -0300

    Add files via upload

--- jogoo.py
+++ jogoo.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 # Inicializando a lista para armazenar as aventuras
 av = []
@@ -23,7 +24,29 @@ def imprime_aventura(copia_v):
 def imprime_escolhida(copia_v, n):
     for k in copia_v:
         if k==copia_v[n]:
-            print(f"av: {k['Aventuras']}")
+            print()
+            print(f"av{n}: {k['Aventuras']}")
+            print()
+            print(f"opção A: {k['opA']}")
+            print(f"opção B: {k['opB']}")
+            escolha = input("Escolha uma opção (A ou B): ").strip().upper()
+            if escolha == 'A':
+                probabilidade_a = k['probSA']
+            elif escolha == 'B':
+                probabilidade_b = k['probSB']
+            else:
+                print("Opção inválida. Tente novamente.")
+                return
+            n_a = random.randint(0,100)
+            if n_a < probabilidade_a:
+                print("msgSA")
+            if n_a > probabilidade_a:
+                print("msgFA")
+            if n_a < probabilidade_b:
+                print("msgSA")
+            if n_a > probabilidade_b:
+                print("msgFA")
+
 
 # Função para excluir uma aventura específica
 def exclui_especifica(copia_v, n):
@@ -44,21 +67,8 @@ print(obj)
 continua("s")
 print("O programa foi retomado.")
 imprime_aventura(av)
-av1 = int(input("escolha uma aventura de 0 a 39 "))
+av1 = random.randint(0, 39)
 print(len(av))  
 imprime_escolhida(av, av1)
 av = exclui_especifica(av, av1)
 print(len(av))
-print("FIM...")
-
-
-
-
-
-
-
-
-
-
-
-
