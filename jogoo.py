ab44d70f8c3eb241f01b850e569db04158d1cd07
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2024-07-27 23:42:09 -0300

    Add files via upload

--- jogoo.py
+++ jogoo.py
@@ -4,7 +4,7 @@ import random
 # Inicializando a lista para armazenar as aventuras
 av = []
 
-# Lendo o conteúdo do arquivo 'objetivo.txt'
+# Lendo o conteúdo do arquivo 'Objetivo.txt'
 with open('Objetivo.txt', 'r') as arquivo:
     obj = arquivo.read()
 
@@ -19,37 +19,33 @@ def imprime_aventura(copia_v):
     for k in copia_v:
         print(f"av: {k['Aventuras']}")
 
-
 # Função para imprimir uma aventura específica
 def imprime_escolhida(copia_v, n):
     for k in copia_v:
-        if k==copia_v[n]:
+        if k == copia_v[n]:
             print()
-            print(f"av{n}: {k['Aventuras']}")
+            print(f"aventura {n}: {k['Aventuras']}")
             print()
-            print(f"opção A: {k['opA']}")
-            print(f"opção B: {k['opB']}")
+            print(f"opção A): {k['opA']}")
+            print(f"opção B): {k['opB']}")
             escolha = input("Escolha uma opção (A ou B): ").upper()
-            n_a = random.randint(0,100)
-            
+            n_a = random.randint(0, 100)
             if escolha == 'A':
                 probabilidade_a = int(k['probSA'])
                 if n_a < probabilidade_a:
-                    print({k['msgSA']})
-                if n_a > probabilidade_a:
-                    print({k['msgFA']})
+                    print(k['msgSA'])
+                else:
+                    print(k['msgFA'])
             elif escolha == 'B':
                 probabilidade_b = int(k['probSB'])
                 if n_a < probabilidade_b:
-                    print({k['msgSB']})
-                if n_a > probabilidade_b:
-                    print({k['msgFB']})
+                    print(k['msgSB'])
+                else:
+                    print(k['msgFB'])
             else:
                 print("Opção inválida. Tente novamente.")
                 return
-            
-
-            
+            continua("s")
 
 
 # Função para excluir uma aventura específica
@@ -65,7 +61,6 @@ def continua(palavra):
             print("Palavra incorreta. Tente novamente.")
     print("Palavra confirmada, autorização concedida")
 
-
 n = 39
 print(obj)
 continua("s")
@@ -74,14 +69,13 @@ imprime_aventura(av)
 av1 = random.randint(0, n)
 print(len(av))  
 
-while n != 0:
+while n > 0:
     imprime_escolhida(av, av1)
-    av1 = random.randint(0, n)
-    print(len(av))  
-    n = n - 1
-
-
-
-
-av = exclui_especifica(av, av1)
-print(len(av))
+    av = exclui_especifica(av, av1)
+    n = len(av)
+    if n > 0:
+        av1 = random.randint(0, n-1)
+    else:
+        print("Parabéns! Você completou todas as aventuras.")
+        break
+    print(len(av))
